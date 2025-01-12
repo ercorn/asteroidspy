803ed13785e83576279c540a21c4ac40d5975d07
rcorn013 <rctexvn@gmail.com>
2025-01-12 18:39:28 -0500

    simplified black screen fill and removed old startup console logging

--- main.py
+++ main.py
@@ -6,15 +6,12 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0, 0, 0))
+        screen.fill((0, 0, 0))
         pygame.display.flip()
 
 
