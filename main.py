eea3868c2baf866de5095487daee44c5316fbdf4
rcorn013 <rctexvn@gmail.com>
2025-01-12 19:13:58 -0500

    added player and draw to screen

--- main.py
+++ main.py
@@ -3,17 +3,21 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000 #pause loop for 1/60th of a second and assign the elapsed time since last call and convert from sec to ms
 
