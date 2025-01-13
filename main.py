3e09ac416f97c54e673deff90f2f0003681cb4bb
rcorn013 <rctexvn@gmail.com>
2025-01-13 06:45:36 -0500

    grouped update and draw calls

--- main.py
+++ main.py
@@ -10,20 +10,26 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+
+    player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        player.update(dt)
+        
+        for member in updatable:
+            member.update(dt)
+        
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        
+        for member in drawable:
+            member.draw(screen)
         
         pygame.display.flip()
         #limit the framerate to 60 fps
