f20d69517601506d9133cb900cd4a838930a965f
rcorn013 <rctexvn@gmail.com>
2025-01-13 06:39:38 -0500

    added ability to move forwards and backwards (w, s)

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
